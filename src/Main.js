c3ad5b5c22627b6e145c372bffb9ffd981269ab0
Maanan <maanan.seduk@gmail.com>
2021-02-12 19:29:47 +0100

    HTML tag to be interprete in JS

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Select from './Components/Select.js'
 import Button from './Components/Button.js'
 import {Link, useHistory} from 'react-router-dom'
+//import axios from 'axios'
 
 function App() {
 
 const [valueDeckName, setValueDeckName] = useState('N2_Bumpo')
 const [valueQuestionDuration, setValueQuestionDuration] = useState('1')
 const [valueAnswerDuration, setValueAnwserDuration] = useState('1')
+const [isCounterHidden,setIsCounterHidden] = useState(false)
+//const [dataDeck, setDataDeck] = useState([])
+
+var lien = 'https://maanan.xyz/panorama/deck_N2_Bunpo.json'
 
 const history = useHistory()
 
 const dataTable = [
     {
-      value:'N1_Bumpo',
-      label: 'fdfds',
-      answer: 'fdsgdfgfd'
+      value:'deck_N1_Bunpo',
+      label: 'N1 Bunpo',
+      //answer: 'fdsgdfgfd'
     },
     {
-      value:'N2_Bumpo',
-      label: 'AAA',
+      value:'deck_N2_Bunpo',
+      label: 'N2 Bunpo',
       answer: 'okokokok'
     }
   ]
+
+const dataIsHidden =[
+  {
+    value: false,
+    label: 'Hide'
+  },
+  {
+    value: true,
+    label: 'Show'
+  },
+]
 
 const valueDuration = [
     {
@@ -30,20 +46,24 @@ const valueDuration = [
       label: '1 second'
     },
     {
-      value: 2,
-      label: '2 seconds'
-    },
-    {
       value: 3,
       label: '3 seconds'
     },
     {
-      value: 4,
-      label: '4 seconds'
-    },
-    {
       value: 5,
       label: '5 seconds'
+    },
+    {
+      value: 7,
+      label: '7 seconds'
+    },
+    {
+      value: 9,
+      label: '9 seconds'
+    },
+    {
+      value: 11,
+      label: '11 seconds'
     }
   ]
 
@@ -59,6 +79,10 @@ const handleChangeAnswerDuration = (event) => {
   setValueAnwserDuration(event.target.value)
 }
 
+const handleChangeIsHidden = (event) => {
+  setIsCounterHidden(event.target.value)
+}
+
 const handleClickButton = () => {
 
   if (valueDeckName !== ''){
@@ -66,19 +90,37 @@ const handleClickButton = () => {
   console.log('on lance le panorama!')
   history.push('/panorama', {
     deckName: valueDeckName,
+    //deckData: dataDeck,
     questionDuration: valueQuestionDuration,
-    answerDuration: valueAnswerDuration})
+    answerDuration: valueAnswerDuration,
+    isCounterHidden: isCounterHidden
+  })
 
   } else {console.log('error...')}
 
 }
-console.log(valueDeckName)
+//console.log(valueDeckName)
 /*
 console.log({
   'deck':valueDeckName,
   'question': valueQuestionDuration,
   'answer': valueAnswerDuration
 })*/
+/*
+useEffect(()=>{
+  console.log('3 - component rendu!')
+  //IL FAUDRAIT RESSORTIR LE FETCH DATA CA PAS DE SENS DE FETCH TOUTES LES SECONDES...
+  async function fetchData(){
+      await axios.get(lien)
+          //.then(response => response.json())
+          //.then(response => console.log(response))
+          .then(response => setDataDeck(response))
+          .catch(error => console.log(error))
+  }
+  fetchData()
+}, [])
+*/
+
 
   return (
 
@@ -104,6 +146,13 @@ console.log({
         data={valueDuration}
         onChange={handleChangeAnswerDuration}
         value={valueAnswerDuration}
+    />
+    <div>Show or hide the counter?</div>
+    <Select
+        label='Counter'
+        data={dataIsHidden}
+        onChange={handleChangeIsHidden}
+        value={isCounterHidden}
     />
     <Button
         label="GO!"
--- src/Panorama.js
+++ src/Panorama.js
@@ -1,39 +1,192 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import deck_N2_Bunpo from './deck_N2_Bunpo.json'
+import deck_N1_Bunpo from './deck_N1_Bunpo.json'
+
+const styles ={
+  questionCSS: {
+    fontSize: '50px',
+    border: '1px solid',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: '#F0F8FF'
+  },
+  answerCSS: {
+    fontSize: '25px',
+    border: '1px solid',
+    backgroundColor: '#FAEBD7',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+  },
+  main:{
+    display: 'flex',
+    flexDirection: 'column',
+    //background: 'red',
+    height: 'auto',
+    padding:'10px'
+  },
+  bouton: {
+    //background:'blue',
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'flex-end'
+
+  }
+}
 
 export default function Panorama(props){
 
-
-var nameDeck = props.location.state.deckName
+var deck = props.location.state.deckName
 var questionDuration = props.location.state.questionDuration
 var answerDuration = props.location.state.answerDuration
+var isCounterHidden = props.location.state.isCounterHidden
+
+switch (deck) {
+
+  case 'deck_N1_Bunpo':
+                var myDeck = deck_N1_Bunpo
+                var longueur = myDeck.notes.length
+                //console.log('la valeur est: ', deck_N1_Bunpo);
+
+                break;
+
+  case 'deck_N2_Bunpo':
+                var myDeck = deck_N2_Bunpo
+                var longueur = myDeck.notes.length
+                //console.log('la valeur est: ', deck_N2_Bunpo);
+
+                break;
+
+  default:
+                var myDeck = deck_N2_Bunpo
+                var longueur = myDeck.notes.length
+                //console.log('la valeur est: ', deck_N2_Bunpo);
+
+}
+
 
 const [counterQuestion, setCounterQuestion] =useState(questionDuration)
+const [questionId, setQuestionId] =useState(0)
+const [isAnswer, setIsAnswer] = useState(false)
 const [counterAnswer, setCounterAnswer] =useState(answerDuration)
 
+if (counterQuestion ==0 && !isAnswer) {
+  setIsAnswer(true)
+  setCounterAnswer(answerDuration)
+
+}
+
+if (counterQuestion ==0 && isAnswer && counterAnswer ==0){
+  setQuestionId(Math.random())
+  setCounterQuestion(questionDuration)
+  setIsAnswer(false)
+}
+
+
+
+
+const handleClick = () => {
+  window.history.back()
+}
+
+
 useEffect(() => {
-  const timer = setTimeout(() => {
-    setCounterQuestion('Timeout called!');
-  }, questionDuration*1000);
-  return () => clearTimeout(timer);
-}, []);
+//console.log('1 - component rendu!')
+const timerQuestion =
+  counterQuestion > 0 && setInterval(() => setCounterQuestion(counterQuestion - 1), 1000);
+/*
+const timerAnswer =
+  counterAnswer > 0 && setInterval(() => setCounterAnswer(counterAnswer - 1), 1000);
+*/
+return () => {
+  clearInterval(timerQuestion);
+  //fetchData();
+  //clearInterval(timerAnswer);
+  };
+
+},[counterQuestion])
+
+
+useEffect(() => {
+//console.log('1 - component rendu!')
+/*const timerQuestion =
+  counterQuestion > 0 && setInterval(() => setCounterQuestion(counterQuestion - 1), 1000);
+*/
+const timerAnswer =
+  counterAnswer > 0 && setInterval(() => setCounterAnswer(counterAnswer - 1), 1000);
 
 
 
-console.log('counter question: ', counterQuestion )
-console.log('counter answer: ', counterAnswer )
+
+
+
+return () => {
+  //clearInterval(timerQuestion);
+  //fetchData();
+  clearInterval(timerAnswer);
+  };
+
+},[counterAnswer])
+
+console.log(
+{
+  deck: deck,
+  counterQuestion: counterQuestion,
+  counterAnswer: counterQuestion,
+  isAnswer: isAnswer,
+  questionId:questionId
+}
+
+)
+
 
   return (
-    <>
 
-    <div>{nameDeck}</div>
-    <div>{questionDuration} second(s) for the questions</div>
-    <div>{answerDuration} second(s) for the answer</div>
+<>
+<div>
+
+        {isCounterHidden &&    <div>
+              counter question: {counterQuestion}<br/>
+              counter réponse: {counterAnswer}
+            </div>}
+
+        {counterQuestion >0 ?
+          <div>{/*counterQuestion} second(s) for the questions - Question ID: {Math.round(questionId*longueur)*/}
+          <br/>
+          <p style={styles.questionCSS}>{myDeck.notes[Math.round(questionId*longueur)].fields[0]}</p>
+          </div>
+          :
+          <p style={styles.questionCSS}>{myDeck.notes[Math.round(questionId*longueur)].fields[0]}</p>
+
+        }
+        <br/>
+        </div>
+
+        <div style={styles.main}>
 
 
 
 
+        {isAnswer ? <div>{/*On affiche la réponse pendant {counterAnswer}*/}
+        <br/>
+        <div style={styles.answerCSS}>{myDeck.notes[Math.round(questionId*longueur)].fields[1]}</div>
+{deck==='deck_N2_Bunpo' ? <div style={styles.answerCSS}>{myDeck.notes[Math.round(questionId*longueur)].fields[2]}</div> : ''}
+{deck==='deck_N2_Bunpo' ? <div style={styles.answerCSS}>{myDeck.notes[Math.round(questionId*longueur)].fields[3]}</div> : ''}
+        </div> : ' '}
 
-    </>
+        <br/>
+
+        <div style={styles.bouton}>
+              <button onClick={handleClick}>
+              Back to menu
+              </button>
+        </div>
+
+</div>
+</>
+
   )
 
 
